a420a3e22f37b68eae4347a6b832de12ced080e7
Petros <petrouil1@hotmail.com>
2018-12-14 18:47:27 +0200

    allaxa to test.py arxeio

--- test.py
+++ test.py
@@ -1,17 +1,13 @@
-print('dose dyo akeraious h exit gia exodo')
-
-x = int(input('dose 1o akeraio: '))
-
-z = int(input('dose 2o akeraio: '))
+import my_test_fun as mtf 
+import matplotlib.pyplot as plt
 
 
-while type(x) != int or type(z) != int:
-    print('dose akeraio h exit gia exodo')
-    x = input('dose 1o akeraio: ')
-    z = input('dose 2o akeraio: ')
-    if x == 'exit' or z == 'exit':
-        break
-else:
-    s = x+z
-    p = x*z
+
+x = mtf.x(a)
+y = mtf.y(b)
+
+s = x + y
+p = x*y
     print('to athroisma einai {} kai to ginomeno {}'.format(s,p))
+
+plt.plot([1,2,3],[1,4,9])
